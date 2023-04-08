c083b61542e649151af6ab340010ad722a75baa3
hualocson <85344481+hualocson@users.noreply.github.com>
2023-04-08 12:20:48 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const { Configuration, OpenAIApi } = require('openai')
+require('dotenv').config()
+
+const app = express()
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
+
+const messages = []
+const user_input =
+  'Tạo 5 câu hỏi trắc nghiệm có 4 đáp án từ văn bản sau: Code quality tool là một loại công cụ phần mềm được sử dụng để kiểm tra chất lượng mã nguồn trong quá trình phát triển phần mềm. Các công cụ này cung cấp các cách tiếp cận khác nhau để đánh giá chất lượng của mã nguồn, bao gồm cả việc phân tích mã nguồn, kiểm tra lỗi, đánh giá khả năng mở rộng, hiệu suất và độ bảo mật.'
+
+messages.push({ role: 'user', content: user_input })
+app.get('/', async (req, res) => {
+  try {
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: messages,
+    })
+    const completion_text = completion.data.choices[0].message.content
+
+    res.status(200).json(completion_text)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+app.listen(2400, () => {
+  console.log('Server started at port 2400')
+})
